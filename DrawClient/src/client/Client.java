3312d932f92fa096b837b3a951ada12d845e56f1
whiwf <leetusoanh@gmail.com>
2021-09-30 15:06:35 +0700

    setup project

---
+++ DrawClient/src/client/Client.java
@@ -0,0 +1,16 @@
+package client;
+
+/**
+ *
+ * @author whiwf
+ */
+public class Client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ DrawServer/src/server/Server.java
@@ -0,0 +1,16 @@
+package server;
+
+/**
+ *
+ * @author whiwf
+ */
+public class Server {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
